e19c63db9e3a66839ff441497e2a58b0fdc19dbe
Nick Wells <wells.nicholas@gmail.com>
2019-01-09 00:15:08 +0000

    add package-level doc

---
+++ strdist/doc.go
@@ -0,0 +1,10 @@
+/*
+
+Package strdist offers various string distance metrics and a Finder for using
+the algorithms to find strings in a population similar to a target string.
+
+There are, additionally, several functions for manipulating NGrams (NGrams
+are used by several distance metrics implemented here).
+
+*/
+package strdist
